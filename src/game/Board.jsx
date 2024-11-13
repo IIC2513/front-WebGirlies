d2ba54a6b6d2ff0e610c8137f7b483cbc6d87bbe
Matilde Galarce <matildegm@uc.cl>
2024-11-13 00:08:14 -0300

    listo lo de las places

--- src/game/Board.jsx
+++ src/game/Board.jsx
@@ -7,35 +7,53 @@ export const GameContext = createContext(null);
 
 export function Board() {
   const [cells, setCells] = useState([]);
-  const [playerName, setPlayerName] = useState("");
+  const [places, setPlaces] = useState([]);
+  
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_BACKEND_URL}/boards/boardData`, {
       params: { boardId: 1 }  // Pasando boardId como parámetro
     })
       .then((response) => {
-        const data = response.data[0]; // Accede al primer objeto de la respuesta
-        setCells(data.cells);
-        setPlayerName(data.Player?.name || ""); // Verifica si existe Player
+        console.log(response.data["places"]);
+        const data = response.data;
+        setCells(response.data["boards"]["0"].cells);
+        setPlaces(response.data["places"]);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-  
-  return (
-    <GameContext.Provider value={{ cells, setCells }}>
-      <h1 className="title">Tablero de {playerName}</h1>
-      <div className="board">
-        <div className="board-row">
-          {cells.map(cell => (
-            <img
-              src={cell.image}
-              id={cell.cellId}
-            />
+
+return (
+  <GameContext.Provider value={{ cells, setCells, places }}>
+    <h1 className="title">Tablero</h1>
+    <div className="board">
+      {cells.map(cell => (
+        <div key={`${cell.x}-${cell.y}`} className="board-cell">
+          <img
+            src={cell.image}
+            alt={`Cell ${cell.x}, ${cell.y}`}
+            className="cell-image"
+          />
+          {places
+            .filter(place => place.doorX === cell.x && place.doorY === cell.y)
+            .map((place, index) => (
+              <img
+                key={index}
+                src={place.image}
+                alt={place.name}
+                className="place-image"
+                style={{
+                  position: "absolute",
+                  left: `${place.doorX}%`, 
+                  top: `${place.doorY}%`,
+                }}
+              />
           ))}
         </div>
-      </div>
-    </GameContext.Provider>
+      ))}
+    </div>
+  </GameContext.Provider>
   );
 }
