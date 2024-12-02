6dd32e5e4f93f48dd181673f716be5aaff1e72a3
XaviJorquera <xaviera.jorquera@uc.cl>
2024-12-02 06:31:20 -0300

    listo

--- src/game/Instructions.jsx
+++ src/game/Instructions.jsx
@@ -1,38 +1,80 @@
 import styles from './instructions.module.css';
 import board from './../assets/images/baord.jpeg';
-import LogoutButton from '../profile/Logout';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../auth/AuthContext'; 
 import Navbar from '../common/Navbar';
+import axios from 'axios';
 
 function Instructions() {
     const { token } = useContext(AuthContext);
+    const [characters, setCharacters] = useState([]);
+    
+    // Decodificar el token y obtener el userId
+    const payloadBase64 = token.split('.')[1];
+    const payload = JSON.parse(atob(payloadBase64));
+    const userId = payload.sub || null;
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/characters`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        .then((response) => {
+            setCharacters(response.data);
+        })
+        .catch((error) => {
+            console.log("Error fetching characters:", error);
+        });
+    }, [token]);
 
     return (
-    <div className={styles.instructions}>
-        <Navbar />
-        <div className={styles.mainInstructions}>
-            <div className={styles.description_container}>
-                <h3 className={styles.title}>How to Play</h3>
-                <p className={styles.subtitle}><strong>Game Start</strong></p>
-                <li className={styles.description}>Each player secretly receives their role and has the opportunity to select their avatar.</li>
-                <li className={styles.description}>The game begins with all players in the center of the board, with clues and tasks distributed across the rooms.</li>
-                <li className={styles.description}>Each player receives an item card.</li>
-                <p className={styles.subtitle}><strong>Game Progression</strong></p>
-                <li className={styles.description}>Players take turns sequentially.</li>
-                <li className={styles.description}>Players collect cards that they must bring to the center to share with other players.</li>
-                <li className={styles.description}>Each player can discard and note clues they believe are true/false based on their assumptions and prior information.</li>
-                <p className={styles.subtitle}><strong>End of the Game</strong></p>
-                <li className={styles.description}>The game ends when one of the following conditions is met:
-                    <li className={styles.description}>The innocent players win if they reveal the impostor's identity or bring all tasks to the center of the board.</li>
-                    <li className={styles.description}>The impostor wins if they remain undiscovered until the end of the game.</li>
-                </li>
+        <div className={styles.instructions}>
+            <Navbar />
+            <h3 className={styles.title}>How to Play</h3>
+            <div className={styles.mainInstructions}>
+                <div className={styles.description_container}>
+                    <p className={styles.subtitle}><strong>Game Start</strong></p>
+                    <li className={styles.description}>Each player has the opportunity to select their avatar.</li>
+                    <li className={styles.description}>The game begins with cards of suspects, weapons, and rooms placed in their respective categories. A card from each category is randomly selected and placed in the confidential envelope, which contains the solution to the case.</li>
+                    <li className={styles.description}>The remaining cards are shuffled and distributed among the players and rooms.</li>
+                    <p className={styles.subtitle}><strong>Game Progression</strong></p>
+                    <li className={styles.description}>Players take turns rolling a die to move around the board towards rooms that contain cards, allowing them to collect them.</li>
+                    <li className={styles.description}>Each player can discard and note clues they believe to be true/false based on their assumptions and prior information.</li>
+                    <p className={styles.subtitle}><strong>Making an Accusation</strong></p>
+                    <li className={styles.description}>When a player believes they know the solution, they can make an accusation. If the accusation is correct, the player wins. If it’s incorrect, the player is eliminated and can no longer play or make accusations.</li>
+                    <p className={styles.subtitle}><strong>End of the Game</strong></p>
+                    <li className={styles.description}>The game ends when one of the following conditions is met:
+                        <li className={styles.description}>A player wins by correctly guessing the suspect, weapon, and room.</li>
+                        <li className={styles.description}>The game continues until a player makes a correct accusation or is eliminated.</li>
+                    </li>
+                    <p className={styles.subtitle}><strong>Eliminating Suspects</strong></p>
+                    <li className={styles.description}>Each card seen or discarded helps deduce which cards are in the confidential envelope, moving players closer to solving the case.</li>
+                </div>
+                <div className={styles.TableroConteinerAbout}>
+                    <img className={styles.imgtablero} src={board} alt="Game board" />
+                </div>
             </div>
-            <img className={styles.imgtablero} src={board}/>
+            <h3 className={styles.title2}>Characters</h3>
+            <div className="character-grid">
+                {characters.map((character) => (
+                    <div className='character-card'>
+                        <img
+                            src={character.avatar}
+                            alt={`${character.name} avatar`}
+                            className={styles.characterAvatar}
+                        />
+                        <div className={styles.characterDescription}>
+                            <h2>{character.name}</h2>
+                            <p>Edad: {character.age}</p>
+                            <p>Objeto distintivo: {character.distinctiveItem}</p>
+                            <p>{character.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-
-    </div>
-    )
+    );
 }
 
-export default Instructions
+export default Instructions;
